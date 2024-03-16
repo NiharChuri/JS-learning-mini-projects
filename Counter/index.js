2e33cb72860be86b09a76cc18ff3df2ba27311de
Nihar <nihar.churi@gmail.com>
2024-03-17 00:01:50 +0530

    Update index.js
    Added keyboard input reading for each button

--- Counter/index.js
+++ Counter/index.js
@@ -9,13 +9,31 @@ increaseBtn.onclick = function(){
     count++;
     counter.textContent = count;
 }
+document.body.addEventListener("keydown", (ev) => {
+    if (ev.key == "Enter"){
+        count++;
+        counter.textContent = count;
+    }
+});
 
 decreaseBtn.onclick = function(){
     count--;
     counter.textContent = count;
 }
+document.body.addEventListener("keydown", (ev) => {
+    if (ev.key == "Backspace"){
+        count--;
+        counter.textContent = count;
+    }
+});
 
 resetBtn.onclick = function(){
     count = 0;
     counter.textContent = count;
 }
+document.body.addEventListener("keydown", (ev) => {
+    if (ev.key == " "){
+        count = 0;
+        counter.textContent = count;
+    }
+});
